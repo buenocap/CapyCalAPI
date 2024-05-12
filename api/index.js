6af77ba1d01e12ead8362176a65500fec54d68cd
Pedro Bueno <pedro.buenocastaneda@ucdenver.edu>
2024-05-12 11:05:20 -0600

    Processing

--- api/index.js
+++ api/index.js
@@ -3,19 +3,39 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@capycaldb.uojru0z.mongodb.net/CapyCalDB?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas", error);
+  });
+
+const Schema = mongoose.Schema;
+const exampleSchema = new Schema({
+  day: String,
+  tasks: Array,
+  id: Number,
+});
+
+const ExampleModel = mongoose.model('Example', exampleSchema);
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-  try {
-    mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@capycaldb.uojru0z.mongodb.net/?retryWrites=true&w=majority&appName=CapyCalDB`
-    );
-  } catch (error) {
-    console.log(error);
+  async function main(){
+    try{
+      const 
+    }
   }
 });
 
